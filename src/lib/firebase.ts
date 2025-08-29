8ac9330726f9e1b37beee0ee5b1ee7a69b3490d5
bjay <bjaybalowna@gmail.com>
2025-08-29 22:01:13 +0000

    this is the firebase realtime database i created:
    // Import the functio

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  "projectId": "complaint-central-mb5pm",
-  "appId": "1:898573072628:web:a5ccc617bf5797c334c6a1",
-  "storageBucket": "complaint-central-mb5pm.firebasestorage.app",
-  "apiKey": "AIzaSyDmKejvRkkUanePoV3I9NVrwF1-j57BdGE",
-  "authDomain": "complaint-central-mb5pm.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "898573072628"
+  apiKey: "AIzaSyCsko2cgUUBZQSARDLTbp8mip0SX62wh74",
+  authDomain: "passengers-map-71296.firebaseapp.com",
+  databaseURL: "https://passengers-map-71296-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "passengers-map-71296",
+  storageBucket: "passengers-map-71296.firebasestorage.app",
+  messagingSenderId: "600735656422",
+  appId: "1:600735656422:web:90974da8fb5ce5fdc84e27",
+  measurementId: "G-0FCR3GDMTR"
 };
 
 const app = initializeApp(firebaseConfig);
